8e737a058488f0cdcd45b295bd120a521552b9c8
Hugh Stuckless <hughstuckless707@gmail.com>
2019-11-10 14:11:31 -0500

    Set up comments

--- bot.js
+++ bot.js
@@ -1,12 +1,16 @@
+// of course, we need the library
 const dcord = require('discord.js');
+// create the bot
 const bot = new dcord.Client();
 
+// run this code when bot is up
 bot.once('ready', () => {
     console.log(`Logged in as ${bot.user.tag} (${bot.user.id}). Run a.help to get started.`)
     bot.user.setActivity('don\'t invite me')
 })
-
+// login
 bot.login('NjQzMTUzODU5MDgyMTkwODk2.Xchedw.CekHiyGODSie-T44P7BVwkvWjwc')
+// run server so heroku works
 require('http').createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain')
